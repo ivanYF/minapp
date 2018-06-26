8513d81325d58c334d4629f96cfadc6bb0b71278
ivan <liuyongivan@126.com>
2018-06-26 17:54:01 +0800

    [inclease:add vertical navbar]

--- dist/pages/navbar/navbar.js
+++ dist/pages/navbar/navbar.js
@@ -1,28 +1,27 @@
 'use strict';
 
 // 获取全局应用程序实例对象
-var app = getApp();
+// const app = getApp()
 
 // 创建页面实例对象
 Page({
     /**
-    * 页面的初始数据
-    */
+     * 页面的初始数据
+     */
     data: {
-        title: 'Index page',
-        userInfo: {},
+        title: 'navbar',
         curIndex: 0,
         category: [{
             name: '推荐',
             id: 1
         }, {
-            name: '护肤',
+            name: '护肤品',
             id: 2
         }, {
             name: '彩妆',
             id: 3
         }, {
-            name: '洗漱',
+            name: '洗漱品',
             id: 4
         }, {
             name: '居家',
@@ -45,27 +44,56 @@ Page({
         }, {
             name: '书籍',
             id: 11
+        }, {
+            name: '推荐',
+            id: 12
+        }, {
+            name: '护肤品',
+            id: 13
+        }, {
+            name: '彩妆',
+            id: 14
+        }, {
+            name: '居家',
+            id: 15
+        }, {
+            name: '服饰',
+            id: 16
+        }, {
+            name: '包包',
+            id: 17
+        }, {
+            name: '化妆',
+            id: 18
+        }, {
+            name: '口红',
+            id: 19
+        }, {
+            name: '防晒',
+            id: 20
+        }, {
+            name: '书籍',
+            id: 21
         }],
-        wWidth: 0,
         wHeight: 0,
-        scrollL: 0
+        scrollT: 0
     },
+
     /**
     * 生命周期函数--监听页面加载
     */
     onLoad: function onLoad() {
-        var _this = this;
-
-        // console.dir(app.data)
-        app.getUserInfo().then(function (info) {
-            return _this.setData({ userInfo: info });
-        }).catch(console.info);
+        // TODO: onLoad
     },
+
 
     /**
     * 生命周期函数--监听页面初次渲染完成
     */
-    onReady: function onReady() {},
+    onReady: function onReady() {
+        // TODO: onReady
+    },
+
 
     /**
     * 生命周期函数--监听页面显示
@@ -73,44 +101,44 @@ Page({
     onShow: function onShow() {
         var wData = wx.getSystemInfoSync();
         this.setData({
-            wWidth: wData.windowWidth,
             wHeight: wData.windowHeight
         });
     },
 
     switchCategory: function switchCategory(e) {
+        console.log(e);
         var idx = e.currentTarget.dataset.index; // 获取点击元素的索引
         this.setData({ curIndex: idx });
-        var offsetL = e.target.offsetLeft; // 点击的item距离适口view左边界距离
+        var offsetT = e.target.offsetTop; // 点击的item距离适口view左边界距离
         /**
          * 获取点击元素距离中心的偏移量
          * 滚动距离 = 偏移数值 - 视口／2 (稍微增加一些元素的中间数值)
          */
-        var scrollL = offsetL - this.data.wWidth / 2 + 20;
-        scrollL = scrollL > 0 ? scrollL : 0;
-        this.setData({ scrollL: scrollL });
-    },
-    scrollFun: function scrollFun(e) {
-        // console.log(e);
+        var scrollT = offsetT - this.data.wHeight / 2 + 40;
+        console.log(scrollT);
+        scrollT = scrollT > 0 ? scrollT : 0;
+        this.setData({ scrollT: scrollT });
     },
     /**
     * 生命周期函数--监听页面隐藏
     */
     onHide: function onHide() {
-        // console.log(' ---------- onHide ----------')
+        // TODO: onHide
     },
+
 
     /**
     * 生命周期函数--监听页面卸载
     */
     onUnload: function onUnload() {
-        // console.log(' ---------- onUnload ----------')
+        // TODO: onUnload
     },
+
 
     /**
     * 页面相关事件处理函数--监听用户下拉动作
     */
     onPullDownRefresh: function onPullDownRefresh() {
-        // console.log(' ---------- onPullDownRefresh ----------')
+        // TODO: onPullDownRefresh
     }
 });
--- src/pages/navbar/navbar.js
+++ src/pages/navbar/navbar.js
@@ -1,14 +1,13 @@
 // 获取全局应用程序实例对象
-const app = getApp()
+// const app = getApp()
 
 // 创建页面实例对象
 Page({
-    /**
-    * 页面的初始数据
-    */
+  /**
+   * 页面的初始数据
+   */
     data: {
-        title: 'Index page',
-        userInfo: {},
+        title: 'navbar',
         curIndex:0,
         category:[
             {
@@ -16,7 +15,7 @@ Page({
                 id:1,
             },
             {
-               name:'护肤',
+               name:'护肤品',
                 id:2, 
             },
             {
@@ -24,7 +23,7 @@ Page({
                 id:3,
             },
             {
-                name:'洗漱',
+                name:'洗漱品',
                 id:4,
             },
             {
@@ -55,66 +54,106 @@ Page({
                 name:'书籍',
                 id:11,
             },
+            {
+                name:'推荐',
+                id:12,
+            },
+            {
+               name:'护肤品',
+                id:13, 
+            },
+            {
+                name:'彩妆',
+                id:14,
+            },
+            {
+                name:'居家',
+                id:15,
+            },
+            {
+                name:'服饰',
+                id:16,
+            },
+            {
+                name:'包包',
+                id:17,
+            },
+            {
+                name:'化妆',
+                id:18,
+            },
+            {
+                name:'口红',
+                id:19,
+            },
+            {
+                name:'防晒',
+                id:20,
+            },
+            {
+                name:'书籍',
+                id:21,
+            },
         ],
-        wWidth: 0,
         wHeight:0,
-        scrollL: 0,
+        scrollT: 0,
     },
+
     /**
     * 生命周期函数--监听页面加载
     */
     onLoad () {
-        // console.dir(app.data)
-        app.getUserInfo()
-        .then(info => this.setData({ userInfo: info }))
-        .catch(console.info)
+    // TODO: onLoad
     },
+
     /**
     * 生命周期函数--监听页面初次渲染完成
     */
     onReady () {
+    // TODO: onReady
     },
+
     /**
     * 生命周期函数--监听页面显示
     */
     onShow () {
         let wData = wx.getSystemInfoSync();
         this.setData({ 
-            wWidth:wData.windowWidth,
             wHeight:wData.windowHeight,
         })
     },
     switchCategory:function(e){
+        console.log(e);
         let idx = e.currentTarget.dataset.index; // 获取点击元素的索引
         this.setData({curIndex:idx}) 
-        let offsetL = e.target.offsetLeft; // 点击的item距离适口view左边界距离
+        let offsetT = e.target.offsetTop; // 点击的item距离适口view左边界距离
         /**
          * 获取点击元素距离中心的偏移量
          * 滚动距离 = 偏移数值 - 视口／2 (稍微增加一些元素的中间数值)
          */
-        let scrollL = offsetL - this.data.wWidth/2 + 20;
-        scrollL = scrollL > 0 ? scrollL : 0;
-        this.setData({ scrollL:scrollL }) 
-    },
-    scrollFun:function(e){
-        // console.log(e);
+        let scrollT = offsetT - this.data.wHeight/2 + 40;
+        console.log(scrollT);
+        scrollT = scrollT > 0 ? scrollT : 0;
+        this.setData({ scrollT:scrollT }) 
     },
     /**
     * 生命周期函数--监听页面隐藏
     */
     onHide () {
-        // console.log(' ---------- onHide ----------')
+    // TODO: onHide
     },
+
     /**
     * 生命周期函数--监听页面卸载
     */
     onUnload () {
-        // console.log(' ---------- onUnload ----------')
+    // TODO: onUnload
     },
+
     /**
     * 页面相关事件处理函数--监听用户下拉动作
     */
     onPullDownRefresh () {
-        // console.log(' ---------- onPullDownRefresh ----------')
+    // TODO: onPullDownRefresh
     }
 })
