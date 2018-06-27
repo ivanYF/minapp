8cd1235fa69173104085108cf4f95812da8a3c82
ivan <liuyongivan@126.com>
2018-06-27 17:48:04 +0800

    [modify: mappage module]

--- dist/pages/mappage/mappage.js
+++ dist/pages/mappage/mappage.js
@@ -5,72 +5,128 @@
 
 // 创建页面实例对象
 Page({
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    title: 'mappage'
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        title: 'mappage',
+        address: '',
+        latitude: '',
+        longitude: '',
+        showButton: false
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function onLoad() {
-    // TODO: onLoad
-  },
-
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function onReady() {
-    // TODO: onReady
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function onLoad() {
+        // TODO: onLoad
+    },
 
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function onShow() {},
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function onReady() {
+        // TODO: onReady
+    },
 
 
-  getLocation: function getLocation() {
-    wx.getLocation({
-      type: 'gcj02', //返回可以用于wx.openLocation的经纬度
-      success: function success(res) {
-        var latitude = res.latitude;
-        var longitude = res.longitude;
-        wx.chooseLocation({
-          latitude: latitude,
-          longitude: longitude,
-          scale: 28,
-          success: function success(res) {
-            console.log(res);
-          }
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function onShow() {
+        this.checkLocationSet();
+    },
+
+    checkLocationSet: function checkLocationSet() {
+        var _this = this;
+        wx.getSetting({
+            success: function success(res) {
+                console.log(res);
+                if (res.authSetting['scope.userLocation'] == null) {
+                    // 未授权过，不做任何处理
+                    _this.setData({ showButton: false });
+                } else {
+                    if (!res.authSetting['scope.userLocation']) {
+                        // 关闭了 应该打开
+                        _this.setData({ showButton: true });
+                    } else {
+                        // 直接处理
+                        _this.setData({ showButton: false });
+                    }
+                }
+            }
         });
-      }
-    });
-  },
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function onHide() {
-    // TODO: onHide
-  },
+    },
+    getLocation: function getLocation() {
+        var _this = this;
+
+        wx.getSetting({
+            success: function success(res) {
+                if (res.authSetting['scope.userLocation'] == null) {
+                    _this.saveLocation();
+                } else {
+                    if (!res.authSetting['scope.userLocation']) {
+                        wx.authorize({
+                            scope: 'scope.userLocation',
+                            success: function success() {
+                                // 成功
+                                _this.saveLocation();
+                            }
+                        });
+                    } else {
+                        // 直接处理
+                        _this.saveLocation();
+                    }
+                }
+            }
+        });
+    },
+    saveLocation: function saveLocation() {
+        var _this = this;
+        wx.getLocation({
+            type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+            success: function success(res) {
+                var latitude = res.latitude;
+                var longitude = res.longitude;
+                wx.chooseLocation({
+                    latitude: latitude,
+                    longitude: longitude,
+                    scale: 28,
+                    success: function success(res) {
+                        console.log(res);
+                        _this.setData({
+                            address: res.address,
+                            latitude: res.latitude,
+                            longitude: res.longitude
+
+                        });
+                    }
+                });
+            }
+        });
+    },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function onHide() {
+        // TODO: onHide
+    },
 
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function onUnload() {
-    // TODO: onUnload
-  },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function onUnload() {
+        // TODO: onUnload
+    },
 
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function onPullDownRefresh() {
-    // TODO: onPullDownRefresh
-  }
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function onPullDownRefresh() {
+        // TODO: onPullDownRefresh
+    }
 });
--- src/pages/mappage/mappage.js
+++ src/pages/mappage/mappage.js
@@ -7,7 +7,11 @@ Page({
      * 页面的初始数据
      */
     data: {
-      title: 'mappage'
+        title: 'mappage',
+        address: '',
+        latitude: '',
+        longitude:'',
+        showButton:false,
     },
 
     /**
@@ -28,10 +32,54 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow () {
-        
+        this.checkLocationSet()
     },
-
+    checkLocationSet:function(){
+        var _this = this;
+        wx.getSetting({
+            success(res) {
+                console.log(res);
+                if (res.authSetting['scope.userLocation'] == null) {
+                    // 未授权过，不做任何处理
+                    _this.setData({ showButton:false })
+                }else{
+                    if (!res.authSetting['scope.userLocation']) {
+                        // 关闭了 应该打开
+                        _this.setData({ showButton:true })
+                    }else{
+                        // 直接处理
+                        _this.setData({ showButton:false })
+                    }   
+                }
+            }
+        })
+    },
     getLocation:function(){
+        var _this = this;
+
+        wx.getSetting({
+            success(res) {
+                if (res.authSetting['scope.userLocation'] == null) {
+                    _this.saveLocation();
+                }else{
+                    if (!res.authSetting['scope.userLocation']) {
+                        wx.authorize({
+                            scope: 'scope.userLocation',
+                            success() {
+                                // 成功
+                                _this.saveLocation();
+                            }
+                        })
+                    }else{
+                        // 直接处理
+                        _this.saveLocation();
+                    }
+                }
+            }
+        })
+    },
+    saveLocation:function(){
+        var _this = this;
         wx.getLocation({
             type: 'gcj02', //返回可以用于wx.openLocation的经纬度
             success: function(res) {
@@ -43,6 +91,12 @@ Page({
                     scale: 28,
                     success:function(res){
                         console.log(res);
+                        _this.setData({
+                            address: res.address,
+                            latitude: res.latitude,
+                            longitude: res.longitude
+
+                        })
                     }
                 })
             }
