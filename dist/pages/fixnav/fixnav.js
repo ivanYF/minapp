78d90ef5c2698d3e21d50340cf3df80727c6055f
ivan <liuyongivan@126.com>
2018-07-18 18:47:25 +0800

    [inclease:fixnav module]

---
+++ dist/pages/fixnav/fixnav.js
@@ -0,0 +1,134 @@
+'use strict';
+
+// 获取全局应用程序实例对象
+// const app = getApp()
+
+// 创建页面实例对象
+Page({
+    /**
+    * 页面的初始数据
+    */
+    data: {
+        title: 'fixnav',
+        category: [[{
+            name: '推荐',
+            id: 1
+        }, {
+            name: '护肤品',
+            id: 2
+        }, {
+            name: '彩妆',
+            id: 3
+        }, {
+            name: '洗漱品',
+            id: 4
+        }, {
+            name: '居家',
+            id: 5
+        }, {
+            name: '服饰',
+            id: 6
+        }], [{
+            name: '包包',
+            id: 7
+        }, {
+            name: '化妆',
+            id: 8
+        }, {
+            name: '口红',
+            id: 9
+        }, {
+            name: '防晒',
+            id: 10
+        }, {
+            name: '书籍',
+            id: 11
+        }, {
+            name: '推荐',
+            id: 12
+        }], [{
+            name: '护肤品',
+            id: 13
+        }, {
+            name: '彩妆',
+            id: 14
+        }, {
+            name: '居家',
+            id: 15
+        }, {
+            name: '服饰',
+            id: 16
+        }, {
+            name: '包包',
+            id: 17
+        }, {
+            name: '化妆',
+            id: 18
+        }], [{
+            name: '口红',
+            id: 19
+        }, {
+            name: '防晒',
+            id: 20
+        }, {
+            name: '书籍',
+            id: 21
+        }, {
+            name: '推荐',
+            id: 22
+        }, {
+            name: '护肤品',
+            id: 23
+        }, {
+            name: '彩妆',
+            id: 24
+        }]]
+    },
+
+    /**
+    * 生命周期函数--监听页面加载
+    */
+    onLoad: function onLoad() {
+        // TODO: onLoad
+    },
+
+
+    /**
+    * 生命周期函数--监听页面初次渲染完成
+    */
+    onReady: function onReady() {
+        // TODO: onReady
+    },
+
+
+    /**
+    * 生命周期函数--监听页面显示
+    */
+    onShow: function onShow() {
+        // TODO: onShow
+    },
+
+
+    /**
+    * 生命周期函数--监听页面隐藏
+    */
+    onHide: function onHide() {
+        // TODO: onHide
+    },
+
+
+    /**
+    * 生命周期函数--监听页面卸载
+    */
+    onUnload: function onUnload() {
+        // TODO: onUnload
+    },
+
+
+    /**
+    * 页面相关事件处理函数--监听用户下拉动作
+    */
+    onPullDownRefresh: function onPullDownRefresh() {
+        // TODO: onPullDownRefresh
+    }
+});
